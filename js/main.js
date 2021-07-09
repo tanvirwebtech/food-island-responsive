5a822e425664792b6091fb83dd64a6b012d5d9c7
Tanvir Ahmed <tanvirahmed9593@gmail.com>
2021-07-09 20:52:19 +0600

    done

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 1,
+            },
+        },
+    });
+    $(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+    $(".owl-next").html('<i class="fa fa-chevron-right"></i>');
+
+    $(".counter").counterUp({
+        delay: 10,
+        time: 1000,
+    });
+});
